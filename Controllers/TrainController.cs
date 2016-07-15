85f492b21d0280ab110def96cb5207163671e441
Mormegil <mormegil@centrum.cz>
2016-07-15 15:59:21 +0200

    Train selection page

--- Controllers/TrainController.cs
+++ Controllers/TrainController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using KdyPojedeVlak.Engine;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,9 +7,26 @@ namespace KdyPojedeVlak.Controllers
 {
     public class TrainController : Controller
     {
-        public IActionResult Index()
+        private static readonly Regex reTrainNumber = new Regex(@"^\s*[A-Z]*\s*(?<id>[0-9]+)\s*$", RegexOptions.CultureInvariant | RegexOptions.ExplicitCapture | RegexOptions.IgnoreCase);
+
+        public IActionResult Index(string search)
         {
-            return View();
+            if (String.IsNullOrEmpty(search)) return View();
+
+            var parsed = reTrainNumber.Match(search);
+            if (!parsed.Success) return View((object)"Nesmyslné zadání. Zadejte číslo vlaku, případně včetně uvedení typu, např. „12345“, „Os 12345“, „R135“ apod.");
+            var id = parsed.Groups["id"].Value;
+            if (String.IsNullOrEmpty(id)) return View((object)"Zadejte číslo vlaku, případně včetně uvedení typu, např. „12345“, „Os 12345“, „R135“ apod.");
+
+            Train train;
+            if (Program.Schedule.Trains.TryGetValue(id, out train))
+            {
+                return RedirectToAction("Details", new { id });
+            }
+            else
+            {
+                return View((object)String.Format("Vlak č. {0} nebyl nalezen.", id));
+            }
         }
 
         public IActionResult Details(string id)
@@ -22,9 +37,7 @@ namespace KdyPojedeVlak.Controllers
             Train train;
             if (!Program.Schedule.Trains.TryGetValue(id, out train))
             {
-                return NotFound();
-                // TODO: Error message
-                //return RedirectToAction("Index");
+                return RedirectToAction("Index", new { search = id });
             }
             return View(train);
         }
