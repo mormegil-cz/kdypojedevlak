9a7708a805235d580e17fa18743116499900552d
Mormegil <mormegil@centrum.cz>
2024-09-18 16:09:23 +0200

    Updated SR70

--- KdyPojedeVlak.Web/Engine/SR70/PointCodebook.cs
+++ KdyPojedeVlak.Web/Engine/SR70/PointCodebook.cs
@@ -33,7 +33,7 @@ namespace KdyPojedeVlak.Web.Engine.SR70
 
             codebook = new Dictionary<string, PointCodebookEntry>();
 
-            CodebookHelpers.LoadCsvData(path, @"SR70-2024-04-15.csv", ';', Encoding.GetEncoding(1250))
+            CodebookHelpers.LoadCsvData(path, @"SR70-2024-10-15.csv", ';', Encoding.GetEncoding(1250))
                 .Select(r => (ID: "CZ:" + r[0].Substring(0, r[0].Length - 1), Row: r))
                 .IntoDictionary(codebook, r => r.ID, r => new PointCodebookEntry
                 {
@@ -46,7 +46,25 @@ namespace KdyPojedeVlak.Web.Engine.SR70
                 });
 
             // add historical data for missing points
+
             /*
+            foreach (var point in CodebookHelpers.LoadCsvData(path, @"SR70-2024-04-15.csv", ';', Encoding.GetEncoding(1250))
+                         .Select(r => (ID: "CZ:" + r[0].Substring(0, r[0].Length - 1), Row: r)))
+            {
+                if (codebook.ContainsKey(point.ID)) continue;
+
+                codebook.Add(point.ID, new PointCodebookEntry
+                {
+                    ID = point.Row[0],
+                    LongName = point.Row[1],
+                    ShortName = point.Row[3],
+                    Type = ParsePointType(point.Row[10]),
+                    Longitude = ParseGeoCoordinate(point.Row[28]),
+                    Latitude = ParseGeoCoordinate(point.Row[29]),
+                });
+
+                DebugLog.LogDebugMsg("Additional point in 2024-04 codebook: {0}", point.ID);
+            }
 
             foreach (var point in CodebookHelpers.LoadCsvData(path, @"SR70-2023-12-10.csv", ';', Encoding.GetEncoding(1250))
                          .Select(r => (ID: "CZ:" + r[0].Substring(0, r[0].Length - 1), Row: r)))
